e3ef94c621abef59f39f7f4dbaded3fd229f042b
Robert Niederreiter <office@squarewave.at>
2014-03-20 13:42:05 +0100

    correct contract

--- src/bda/plone/discount/calculator.py
+++ src/bda/plone/discount/calculator.py
@@ -2,7 +2,6 @@ from zope.interface import Interface
 from zope.interface import implementer
 from zope.component import adapter
 from bda.plone.cart.interfaces import ICartItem
-from bda.plone.cart.interfaces import IDiscount
 from bda.plone.cart.interfaces import ICartDiscount
 from bda.plone.cart.interfaces import ICartItemDiscount
 from bda.plone.discount.interfaces import ICartItemDiscountSettings
@@ -13,8 +12,7 @@ from bda.plone.discount.interfaces import IUserCartDiscountSettings
 from bda.plone.discount.interfaces import IGroupCartDiscountSettings
 
 
-@implementer(IDiscount)
-class Discount(object):
+class DiscountBase(object):
 
     def __init__(self, context):
         self.context = context
@@ -22,18 +20,21 @@ class Discount(object):
     def aggregated_rules(self):
         pass
 
-    def reduced_net(self, net, vat):
-        print 'Discount.reduced_net'
-        return net
+
+@implementer(ICartDiscount)
+@adapter(Interface)
+class CartDiscount(DiscountBase):
+
+    def net(self, items):
+        return 0.0
+
+    def vat(self, items):
+        return 0.0
 
 
 @implementer(ICartItemDiscount)
 @adapter(ICartItem)
-class CartItemDiscount(Discount):
-    pass
+class CartItemDiscount(DiscountBase):
 
-
-@implementer(ICartDiscount)
-@adapter(Interface)
-class CartDiscount(Discount):
-    pass
+    def net(self, net, vat):
+        return 0.0
